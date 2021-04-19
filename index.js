a27f9bb107aec00458c438e090a9b8e93480d6f9
Sophie Shin <sophie@alloy.co>
2021-04-19 20:18:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ GAME.appendChild(rock)
      * we should remove the rock from the DOM.
      */
   }
-
+window.requestAnimationFrame(moveRock)
   // We should kick off the animation of the rock around here.
 
   // Add the rock to ROCKS so that we can remove all rocks
